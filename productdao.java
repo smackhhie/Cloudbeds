b479ddce8e886bb538c49f4b1b658bc1772531ab
Pradip-Lamsal <121224303+Pradip-Lamsal@users.noreply.github.com>
2023-07-05 21:27:47 +0545

    Add files via upload

---
+++ productdao.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author Craze
+ */
+public class productdao {
+    
+}
+
+public static ArrayList<Product> filterProductByname(String name, String category) {
+
+}
