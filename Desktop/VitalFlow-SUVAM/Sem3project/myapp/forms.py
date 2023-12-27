3791f1e4c267568d97497b9bfaac0989a7f5d2e3
Ayush Bhetwal <rujanbhetwal65@gmail.com>
2023-12-27 19:03:59 +0545

    Backend for add techniacian

---
+++ Desktop/VitalFlow-SUVAM/Sem3project/myapp/forms.py
@@ -0,0 +1,37 @@
+#forms.py
+from django import forms
+from .models import Report_Detail  # Import your ReportDetail model here
+
+class Report_DetailForm(forms.ModelForm):
+    class Meta:
+        model = Report_Detail  # Replace ReportDetail with your model name
+<<<<<<< HEAD
+        fields = '__all__'
+
+        # fields=['test_list']# Include all fields or specify the fields you need
+=======
+        fields = ['test_list']
+
+        # fields=['test_list']# Include all fields or specify the fields you need
+
+
+# =============================================suvam ko part
+
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
+
+class EditadminprofileForm(UserChangeForm):
+    password=None
+    class Meta:
+        model =User
+        fields = ['username','first_name','last_name','email','date_joined','last_login','is_active']
+        labels={'email': 'Email' }
+
+
+class EditsuperadminprofileForm(UserChangeForm):
+    password=None
+    class Meta:
+        model =User
+        fields = '__all__'
+        labels={'email': 'Email' }
+>>>>>>> origin/ayushbhetwal
---
+++ Desktop/VitalFlow-SUVAM/Sem3project/myapp/urls.py
@@ -0,0 +1,40 @@
+# urls.py
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+   path("",views.index,name='myapp'),
+   path("about",views.about,name='about'),
+   path("services",views.index,name='myapp'),
+   path("home",views.home, name='home'),
+   path("availabletest",views.availabletest, name='availabletest'),
+   path("package",views.package, name='package'),
+   path("report",views.report, name='report'),
+   path("feedback",views.feedback, name='feedback'),
+   path("contact",views.contact, name='contact'),
+<<<<<<< HEAD
+   path("adminpage",views.adminpage, name='adminpage'),
+   path("createreport",views.createreport,name='createreport'),
+    path("techlogin",views.techlogin, name='techlogin'),
+    path("packages",views.packages, name='packages'),
+ path("viewreport",views.viewreport,name='viewreport'),
+ path("updatereport/<str:contact>/",views.updatereport,name='updatereport'),
+ 
+=======
+   # path("adminprofile",views.adminprofile, name='adminprofile'),
+   path("createreport",views.createreport,name='createreport'),
+   #  path("techlogin",views.techlogin, name='techlogin'),
+    path("packages",views.packages, name='packages'),
+ path("viewreport",views.viewreport,name='viewreport'),
+
+#  path("adminlogin",views.adminlogin,name='adminlogin'),
+path('adminlogin/',views.admin_login,name ='adminlogin'),
+path('adminlogout/',views.admin_logout,name ='adminlogout'),
+path('adminprofile/',views.admin_profile,name='adminprofile'),
+path('changepassword/',views.admin_password,name='changepassword'),
+
+
+path("updatereport/<str:contact>/",views.updatereport,name='updatereport'),
+>>>>>>> origin/ayushbhetwal
+ ]
